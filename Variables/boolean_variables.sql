60f38eee552cf0f0df87c9beb2e976a5b2b319b7
Stefanescu Mircea <stefanescumircea1990@gmail.com>
2023-09-12 18:15:16 +0300

    variables

---
+++ Variables/boolean_variables.sql
@@ -0,0 +1,10 @@
+set SERVEROUTPUT ON;
+----------------==================================---------------
+----------------USING BOOLEAN DATA TYPE in PL/SQL----------------
+----------------==================================---------------
+DECLARE
+    v_boolean boolean := true;
+BEGIN
+    dbms_output.put_line(sys.diutil.bool_to_int(v_boolean));
+END;
+----------------==================================---------------
